b692c0d919fba00f6bd40665f07af79f60200de0
Masaaki Demura <masaaki.demura@nexture-tech.com>
2014-07-27 19:42:59 +0900

    Migrated from maven to gradle

--- src/main/java/jp/co/cachet/quickfix/application/InitiatorApplication.java
+++ src/main/java/jp/co/cachet/quickfix/application/InitiatorApplication.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class InitiatorApplication extends AbstractApplication {
+	@SuppressWarnings("unused")
 	private static final Logger log = LoggerFactory.getLogger(InitiatorApplication.class);
 
 }
